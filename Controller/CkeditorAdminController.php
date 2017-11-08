842174f9e295fa7c0c6c5184405a0c28a11d88bd
Jordi Sala Morales <jordism91@gmail.com>
2017-11-08 23:10:13 +0100

    getRuntime now receives a non deprecated runtime

--- Controller/CkeditorAdminController.php
+++ Controller/CkeditorAdminController.php
@@ -13,6 +13,9 @@ namespace Sonata\FormatterBundle\Controller;
 
 use Sonata\MediaBundle\Controller\MediaAdminController;
 use Sonata\MediaBundle\Provider\MediaProviderInterface;
+use Symfony\Bridge\Twig\AppVariable;
+use Symfony\Bridge\Twig\Extension\FormExtension;
+use Symfony\Component\Form\FormRenderer;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -187,12 +190,11 @@ class CkeditorAdminController extends MediaAdminController
         $twig = $this->get('twig');
 
         // BC for Symfony < 3.2 where this runtime does not exists
-        if (!method_exists('Symfony\Bridge\Twig\AppVariable', 'getToken')) {
-            $twig->getExtension('Symfony\Bridge\Twig\Extension\FormExtension')
-                ->renderer->setTheme($formView, $theme);
+        if (!method_exists(AppVariable::class, 'getToken')) {
+            $twig->getExtension(FormExtension::class)->renderer->setTheme($formView, $theme);
 
             return;
         }
-        $twig->getRuntime('Symfony\Bridge\Twig\Form\TwigRenderer')->setTheme($formView, $theme);
+        $twig->getRuntime(FormRenderer::class)->setTheme($formView, $theme);
     }
 }
--- Tests/Controller/CkeditorAdminControllerTest.php
+++ Tests/Controller/CkeditorAdminControllerTest.php
@@ -14,6 +14,10 @@ namespace Sonata\FormatterBundle\Tests\Controller;
 use PHPUnit\Framework\TestCase;
 use Prophecy\Argument;
 use Sonata\FormatterBundle\Controller\CkeditorAdminController;
+use Symfony\Bridge\Twig\AppVariable;
+use Symfony\Bridge\Twig\Extension\FormExtension;
+use Symfony\Bridge\Twig\Form\TwigRenderer;
+use Symfony\Component\Form\FormRenderer;
 
 class EntityWithGetId
 {
@@ -150,22 +154,21 @@ class CkeditorAdminControllerTest extends TestCase
     private function configureSetFormTheme($formView, $formTheme)
     {
         $twig = $this->prophesize('\Twig_Environment');
-        $twigRenderer = $this->prophesize('Symfony\Bridge\Twig\Form\TwigRenderer');
-
         $this->container->get('twig')->willReturn($twig->reveal());
 
         // Remove this trick when bumping Symfony requirement to 3.2+.
-        if (method_exists('Symfony\Bridge\Twig\AppVariable', 'getToken')) {
-            $twig->getRuntime('Symfony\Bridge\Twig\Form\TwigRenderer')->willReturn($twigRenderer->reveal());
+        if (method_exists(AppVariable::class, 'getToken')) {
+            $twigRenderer = $this->prophesize(FormRenderer::class);
+            $twig->getRuntime(FormRenderer::class)->willReturn($twigRenderer->reveal());
+            $twigRenderer->setTheme($formView, $formTheme)->shouldBeCalled();
         } else {
-            $formExtension = $this->prophesize('Symfony\Bridge\Twig\Extension\FormExtension');
+            $twigRenderer = $this->prophesize(TwigRenderer::class);
+            $formExtension = $this->prophesize(FormExtension::class);
             $formExtension->renderer = $twigRenderer->reveal();
 
-            // This Throw is for the CRUDController::setFormTheme()
-            $twig->getRuntime('Symfony\Bridge\Twig\Form\TwigRenderer')->willThrow('\Twig_Error_Runtime');
-            $twig->getExtension('Symfony\Bridge\Twig\Extension\FormExtension')->willReturn($formExtension->reveal());
+            $twig->getExtension(FormExtension::class)->willReturn($formExtension->reveal());
+            $twigRenderer->setTheme($formView, $formTheme)->shouldBeCalled();
         }
-        $twigRenderer->setTheme($formView, $formTheme)->shouldBeCalled();
     }
 
     private function configureRender($template, $data, $rendered)
